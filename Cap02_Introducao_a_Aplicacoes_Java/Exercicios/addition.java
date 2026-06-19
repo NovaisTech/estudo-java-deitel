1aec9c89c66b9bbe72b50a20acbcfc13b0d04585
Renato Novais <renatonovais.ps@gmail.com>
2026-06-19 01:21:09 -0300

    "Initial commit: Estrutura de estudos Java Deitel"

--- Cap02_Introducao_a_Aplicacoes_Java/Exercicios/addition.java
+++ Cap02_Introducao_a_Aplicacoes_Java/Exercicios/addition.java
@@ -1,28 +1,30 @@
 package Cap02_Introducao_a_Aplicacoes_Java.Exercicios;
 
-import java.util.Scanner;
+import java.util.Scanner; //utiliza a classe Scanner
 
 public class addition {
     
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+			int number1;
+			int number2;
+			int sum;
 
-        int number1;
-        int number2;
-        int sum;
+			System.out.println("Entre o primeiro número");
+			number1 = input.nextInt();
+			
+			System.out.println("Entre o segundo número");
+			number2 = input.nextInt();
+			
+			sum = number1 + number2;
 
-        System.out.println("Entre o primeiro número");
-        number1 = input.nextInt();
-        
-        System.out.println("Entre o segundo número");
-        number2 = input.nextInt();
-        
-        sum = number1 + number2;
-
-        System.out.printf("Soma é %d%n",sum);
+			System.out.printf("Soma é %d%n",sum);
+		}
 
 
 
     }   
 }
+
+//Continua pág 37
